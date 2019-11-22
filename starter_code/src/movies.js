1f9b259e5991149aa7c900f33703e7878ac4a8fe
David Aguero <22003@columbushs.com>
2019-11-22 08:43:07 -0500

    unfinsihed

--- starter_code/src/movies.js
+++ starter_code/src/movies.js
@@ -3,19 +3,61 @@
 // Iteration 1: Ordering by year - Order by year, ascending (in growing order)
 
 
-function orderByYear(movies){
-    return movies.sort();
-    
+function orderByYear(anArray){
+
+   let copy = [...anArray];
+
+   copy.sort((a ,b) =>{
+    if(a.year < b.year){
+        return -1;
+    }
+    else if(b.year < a.year){
+        return 1;
+    }
+    else{
+        if(a.title < b.title){
+            return -1;
+        }
+        else if(b.title > a.title){
+            return 1;
+        }
+        else{
+            return 0;
+        }
+    }
+   })
+   return copy;
+
 }
-
-movies.year.sort()
-
 // Iteration 2: Steven Spielberg. The best? - How many drama movies did STEVEN SPIELBERG direct
 
-function howManyMovies(anArray){
+function howManyMovies([anArray]){
+
 }
 
 // Iteration 3: Alphabetic Order - Order by title and print the first 20 titles
+function orderAlphabetically(anArray){
+    let copy = [...anArray];
+
+    copy.sort((a,b) =>{
+        if(a.title < b.title){
+            return -1;
+        }
+        else if(b.title < a.title){
+            return 1;
+        }
+        else{
+            return 0;
+        }
+        
+    })
+
+
+
+    let firstNum = copy.slice(0, 20);
+
+    return copy;
+}
 
 // Iteration 4: All rates average - Get the average of all rates with 2 decimals
 
